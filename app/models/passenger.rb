d5f913ae3a10cf0c3db79bbd5085494ce865dbe4
ciara-picou <ciarapicou@att.net>
2020-08-31 11:19:30 -0500

    submission for deadline

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,43 @@
 
+class Passenger
+    attr_reader: :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.name
+        @name
+    end
+
+    def self.rides
+    #Returns an array of Ride instances 
+    #that this person has been on
+    Ride.all.select{|ride|Ride.passenger = self}
+    end
+    
+    def self.drivers
+    #Returns an array of Driver instances
+    # that this person has rode with
+    Driver.all.map do |driver|
+        my_drivers = []
+        if Driver.passenger == self
+            my_drivers << self
+        end
+    my_drivers
+    end
+    end
+
+    def self.premium_members
+    #Returns an array of all Passengers 
+    #who have travelled over 100 miles in 
+    #total with the service
+
+    end
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,42 @@
+class Ride
+    attr_accessor :passenger, :driver, :distance
 
+    @@all = []
+
+    def initialize(passenger, driver, distance)
+        @passenger = passenger
+        @driver = driver
+        @distance = distance.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.passenger
+        @passenger
+    end
+
+    def self.driver
+        @driver
+    end
+
+    def self.distance
+        @distance
+    end
+
+    def self.average_distance
+        #Returns the average distance across 
+        #ALL rides
+        self.all.distance.inject(:+) / self.distance.all.length
+    end
+end
+
+#When time is called
+#git add .
+#git commit -m 'submission for deadline'
+#git push origin <your-name> - this command must be ran while still in the branch <your-name>
+#You will never be able to push to master; you will not be able to push to your own branch before time is called.
+#Make a pull request from YOUR branch to master, by visiting the github project repository and clicking the green button: "Submit Pull Request"
+#Copy the URL (from the address bar) to the pull request, and paste the URL into the submission text box in https://learn.co/assignments in the task for this code challenge.
