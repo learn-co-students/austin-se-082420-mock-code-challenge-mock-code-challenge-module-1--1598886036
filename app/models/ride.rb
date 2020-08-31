99a8a8a8907c33cc762ee14d06f725e471f91c76
hproctor021 <hproctor021@gmail.com>
2020-08-31 11:27:06 -0500

    submission for deadline

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,37 @@
+class Ride
+    attr_accessor :driver, :passenger, :distance
 
+    @@all = []
+    def initialize (driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def passenger
+        @passenger
+    end
+
+    def driver
+        @driver
+    end
+
+    def distance
+        @distance
+    end
+
+    def self.all
+        @@all
+    end
+
+    # def self.average_distance
+    #     average = 0 
+    #     Ride.all.select do |r|
+    #         r.distance
+    #         sum = sum + r.distance
+    
+    #      end
+    #     sum / (Ride.all.count).to_f
+    # end
+end
