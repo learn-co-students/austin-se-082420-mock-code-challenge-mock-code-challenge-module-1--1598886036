11b4c2edf1e53d0dc716f58f37a22d3f054bbba9
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-08-31 11:24:07 -0500

    submission for deadline

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,38 @@
+require 'pry'
+# #### Driver
+# - `Driver#passenger_names`
+#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
+# - `Driver#rides`
+#   - Returns an array of all Rides a driver has made
+# - `Driver.mileage_cap(distance)`
+#   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
 
+class Driver
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        @@all << self
+    end
+
+    def self.name
+        self.name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.passenger_names
+        binding.pry
+    end
+
+    def self.rides
+        self.collect do |ride|
+            binding.pry
+        end
+    end
+
+end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+# #### Passenger
+
+# - `Passenger#rides`
+#   - Returns an array of Ride instances that this person has been on
+# - `Passenger#drivers`
+#   - Returns an array of Driver instances that this person has rode with
+# - `Passenger#total_distance`
+#   - Returns the floating number that represents the total distance the passenger has travelled using the service
+# - `Passenger.premium_members`
+#   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
+
+class Passenger 
+    attr_accessor
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.name
+        self.name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.rides
+        binding.pry
+        puts "Hi"
+    end
+
+
+
+end
